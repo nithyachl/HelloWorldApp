77b5e3a52f17671bb1c03373e8bd4d9f5d84b228
Nithya Chintalapati <nithya.ch333@gmail.com>
2020-01-31 13:20:43 -0500

    Initial commit

---
+++ app/src/main/java/com/chintalapatinithya/firstapp/helloworldapp/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.chintalapatinithya.firstapp.helloworldapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HelloWorld extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello_world);
+
+     System.out.println("Hello World");
+
+
+    }
+}
